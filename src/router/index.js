73dac783fece19d61d6e324ca8fcbacc13b83af3
ByDefalt <rousvalromain@gmail.com>
2025-03-05 15:13:40 +0100

    create manager folder in components

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import ManagerList from '../components/ManagerList.vue';
-import ManagerDetails from '../components/ManagerDetails.vue';
-import ManagerForm from '../components/ManagerForm.vue';
+import ManagerList from '../components/manager/ManagerList.vue';
+import ManagerDetails from '../components/manager/ManagerDetails.vue';
+import ManagerForm from '../components/manager/ManagerForm.vue';
 
 const routes = [
     { path: '/managers', component: ManagerList },
